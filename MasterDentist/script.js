037cd7b46337baa2f90ff45f759189cbc9265262
Jacky <jacky.wu@concords.com.tw>
2017-08-01 18:35:16 +0800

    20170801

--- MasterDentist/script.js
+++ MasterDentist/script.js
@@ -18,10 +18,45 @@ When you're done, you can delete all of this grey text, it's just a comment.
 //
 //greetMe("World");
 function sidebar_open() {
-  document.getElementById("sidebar").style.display = "block";
+//  document.getElementById("sidebar").style.display = "block";
+  document.getElementById("sidebar").style.right = 0;
   document.getElementById("sidebarbutton").style.display = "none";
+  document.getElementById("articleblock").style.left = -100 + "px";
+  document.getElementById("footblock").style.left = -100 + "px";
+  // document.getElementById("wholepage").style.left = -100 + "px";
 }
 function sidebar_close() {
-  document.getElementById("sidebar").style.display = "none";
+//  document.getElementById("sidebar").style.display = "none";
+  document.getElementById("sidebar").style.right = -150 + "px";
   document.getElementById("sidebarbutton").style.display = "block";
+  document.getElementById("articleblock").style.left = 0;
+  document.getElementById("footblock").style.left = 0;
+  // document.getElementById("wholepage").style.left = 0;
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
 }
